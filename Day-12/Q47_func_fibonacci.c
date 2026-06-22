908423938bd966183a5b80e71629f976ea90f58a
Ayush Sharma <ayush.sharmaa@hotmail.com>
2026-06-22 21:54:01 +0530

    Day-12 uploaded

---
+++ Day-12/Q47_func_fibonacci.c
@@ -0,0 +1,29 @@
+/*
+ * Q47: Function for Fibonacci
+ * Day-12 | Admission No: 25CSAIM290
+ */
+
+#include <stdio.h>
+
+void printFibonacci(int n) {
+    long long a = 0, b = 1, next;
+    printf("Fibonacci Series: ");
+    for (int i = 1; i <= n; i++) {
+        printf("%lld ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printFibonacci(n);
+
+    return 0;
+}
