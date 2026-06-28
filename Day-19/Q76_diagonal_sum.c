35b159f8e24f1891fd2d16e5e34242a363390288
Ayush Sharma <ayush.sharmaa@hotmail.com>
2026-06-28 22:14:55 +0530

    Day-19 uploaded

---
+++ Day-19/Q76_diagonal_sum.c
@@ -0,0 +1,40 @@
+/*
+ * Q76: Find diagonal sum of a square matrix
+ * Day-19 | Admission No: 25CSAIM290
+ */
+
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter order of square matrix: ");
+    scanf("%d", &n);
+
+    int mat[n][n];
+
+    printf("Enter elements of matrix:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &mat[i][j]);
+
+    int primarySum   = 0;
+    int secondarySum = 0;
+
+    for (int i = 0; i < n; i++) {
+        primarySum   += mat[i][i];
+        secondarySum += mat[i][n - 1 - i];
+    }
+
+    /* If n is odd, the center element is counted twice */
+    int totalDiagonalSum = primarySum + secondarySum;
+    if (n % 2 != 0) {
+        totalDiagonalSum -= mat[n / 2][n / 2];
+    }
+
+    printf("Primary diagonal sum   = %d\n", primarySum);
+    printf("Secondary diagonal sum = %d\n", secondarySum);
+    printf("Total diagonal sum     = %d\n", totalDiagonalSum);
+
+    return 0;
+}
